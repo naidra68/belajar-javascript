7673aa6eb1844a632c642ef9acf63b8f6908026e
naidra68 <ardian.crazy@gmail.com>
2025-06-21 09:02:19 +0700

    edit html, add comparison

---
+++ javascript/04-comparison.js
@@ -0,0 +1,13 @@
+var a = 10;
+var b = 10;
+var ab = a == b;
+var not = a != b;
+console.log(ab); // true
+console.log(not); // false
+
+var x = 10;
+var y = "10";
+var xy = x == y;
+var strict = x === y;
+console.log(xy); // true
+console.log(strict) // false
