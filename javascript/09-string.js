0df8c72f4f29b18bb8ffd2a23b3d4eba350670bb
naidra68 <ardian.crazy@gmail.com>
2025-06-21 18:39:30 +0700

    add string and edit index

---
+++ javascript/09-string.js
@@ -0,0 +1,17 @@
+console.log("Naidra68");
+console.log('Tri Ardian');
+// console.log('"Gerakan Pungut Sampah" dilakukan pada hari Jum'at');
+// console.log(""Gerakan Pungut Sampah" dilakukan pada hari Jum'at");
+console.log('"Gerakan Pungut Sampah" dilakukan pada hari Jum\'at');
+console.log("\"Gerakan Pungut Sampah\" dilakukan pada hari Jum\'at");
+console.log('Ini adalah hari Jum\'at');
+console.log('Ini adalah hari \n Jum\'at');
+console.log('Ini adalah hari \n\n Jum\'at');
+console.log('Ini adalah hari \t Jum\'at');
+console.log('\u00A9');
+console.log('\u2122');
+console.log('Tri Ardian' == 'tri ardian'); // false
+console.log('Tri Ardian' == 'Tri Ardian'); // true
+console.log('Tri Ardian'.length); //10
+
+
