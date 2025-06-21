0fe8fab112537416843b3816873bcb83cbecfc00
naidra68 <ardian.crazy@gmail.com>
2025-06-21 20:59:22 +0700

    add variable and edit index

---
+++ javascript/11-variable.js
@@ -0,0 +1,11 @@
+// Deklarasi variable
+var x;
+console.log(x);
+
+// shorthand variable
+var nama = "naidra",
+    umur = 11,
+    lulus = true;
+
+// variable scope
+// Pelajari dulu Function dan Object baru ke sini lagi.
