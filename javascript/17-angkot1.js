64d674d2ee133425b96a8c248968e9e7ffe04f2d
naidra68 <ardian.crazy@gmail.com>
2025-06-21 22:54:48 +0700

    add angkot1 and edit index

---
+++ javascript/17-angkot1.js
@@ -0,0 +1,13 @@
+// var nilaiAwal = 1;
+// while (nilaiAwal <= 10) {
+//     console.log('Angkot No. ' + nilaiAwal + ' beroperasi dengan baik.');
+//     nilaiAwal++;
+// }
+
+var nilaiAwal = 1,
+    nilaiAkhir = 2;
+while (nilaiAwal <= 20) {
+    console.log('angkot no. ' + nilaiAwal + ' membawa penumpang dengan jumlah ' + nilaiAkhir + ' orang');
+    nilaiAwal++;
+    nilaiAkhir = nilaiAkhir + 2;
+}
