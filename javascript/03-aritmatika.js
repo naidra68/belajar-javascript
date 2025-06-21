fffdda006d6fd6dd4d539ed1813412deda219f83
naidra68 <ardian.crazy@gmail.com>
2025-06-21 08:49:52 +0700

    add 03-aritmatika and edit index

---
+++ javascript/03-aritmatika.js
@@ -0,0 +1,18 @@
+// Operator aritmatika
+var a = 10 + 5;
+var b = 10 - 5;
+var c = 10 * 5;
+var d = 10 / 5;
+var e = 10 % 5;
+console.log(a) // 15
+console.log(b) // 5
+console.log(c) // 50
+console.log(d) // 2
+console.log(e) // 0
+
+// Operator preceden
+// KuKaBaTaKu | (),*,/,+,-
+var f = 1 + 2 * 3 - 4
+console.log(f); // 3
+var g = (1 + 2) * 3 - 4
+console.log(g); // 5
