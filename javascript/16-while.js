0557d9bf0d0916353250baa46d3eb7526e08db8f
naidra68 <ardian.crazy@gmail.com>
2025-06-21 22:44:23 +0700

    add while and edit html

---
+++ javascript/16-while.js
@@ -0,0 +1,14 @@
+// Loop dihentikan oleh user
+
+// var ulang = true;
+// while (ulang) {
+//     console.log('Hello World');
+//     ulang = confirm('lagi?');
+// }
+
+// Loop dihentikan oleh program
+nilaiAwal = 1;
+while (nilaiAwal <= 10) {
+    console.log('Hello World! ' + nilaiAwal + 'x');
+    nilaiAwal++;
+}
