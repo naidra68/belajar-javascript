d9dcdaa5593014a72f1638d8a3c3650808ed19a2
naidra68 <ardian.crazy@gmail.com>
2025-06-21 21:30:54 +0700

    add prompt_box and edit html

---
+++ javascript/14-prompt_box.js
@@ -0,0 +1,12 @@
+// prompt('Masukkan Nilai:');
+
+var nama = prompt('Masukkan nama: ');
+alert(nama);
+var umur = prompt('Masukkan umur: ');
+alert(umur);
+var kerja = prompt('Masukkan pekerjaan: ');
+alert(kerja);
+alert( 'Nama: ' + nama + '\n' +
+       'Umur: ' + umur + '\n' +
+       'Pekerjaan: ' + kerja
+);
