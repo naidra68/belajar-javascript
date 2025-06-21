a8210d3069dc60d7ff7494949971d6c266fe72b5
naidra68 <ardian.crazy@gmail.com>
2025-06-21 19:05:33 +0700

    rdit index and add boolean

---
+++ javascript/10-boolean.js
@@ -0,0 +1,11 @@
+console.log( 10 < 20);
+console.log("Naidra68" == "naidra68");
+console.log(Boolean(true));
+console.log(Boolean(false));
+console.log(Boolean(10 < 20)); // true 10 kurang dari 20
+console.log(Boolean(10)); // true
+console.log(Boolean(0)); // false
+console.log(Boolean(-10)); // true
+
+// Javascript memiliki truthy and falsy
+// Baca README untuk mengetahui tabel truthy and falsy
