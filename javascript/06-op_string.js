69d947c92457cd6c1df57ebbb2771ffa4e5b71e1
naidra68 <ardian.crazy@gmail.com>
2025-06-21 18:15:18 +0700

    add op_string and edit index

---
+++ javascript/06-op_string.js
@@ -0,0 +1,6 @@
+console.log("apple" + "pen"); // applepen
+console.log("apple" + " " + "pen"); // apple pen
+console.log(10 + "10"); // 1010
+console.log("10" + 10); // 1010
+console.log(10 + 10 + "10"); // 2010
+console.log("10" + 10 + 10); // 101010
