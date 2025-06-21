fd0696484e296831c5ee8ceb5f47df9fb18f0272
naidra68 <ardian.crazy@gmail.com>
2025-06-21 17:36:02 +0700

    edit index and comparison

--- javascript/04-comparison.js
+++ javascript/04-comparison.js
@@ -11,3 +11,24 @@ var xy = x == y;
 var strict = x === y;
 console.log(xy); // true
 console.log(strict) // false
+
+var c = 5;
+var d = 10;
+var cd = c < d;
+var dc = c > d;
+console.log(cd); // true
+console.log (dc); // false
+
+var e = 5;
+var f = 5;
+var ef = e <= f;
+var fe = e >= f;
+console.log(ef); // true
+console.log(fe); // true
+
+var g = 5;
+var h = 6;
+var gh = g <= h;
+var hg = g >= h;
+console.log(gh); // true
+console.log(hg); // false
