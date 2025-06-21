f4c529030927148de29f40cd7b4e0436006f8cdc
naidra68 <ardian.crazy@gmail.com>
2025-06-22 00:07:32 +0700

    add if and edit index

---
+++ javascript/20-if.js
@@ -0,0 +1,16 @@
+// var angka = 3;
+
+// if (angka === 1) {
+//     alert('anda memasukkan angka 1');
+// }
+
+// if (angka < 5) {
+//     alert(angka + ' lebih kecil dari 5');
+// }
+
+var angka = prompt('Masukkan Angka : ');
+if (angka % 2 == 0) {
+    alert(angka + ' adalah bilangan GENAP');
+}else {
+    alert(angka + ' adalah bilangan GANJIL');
+}
