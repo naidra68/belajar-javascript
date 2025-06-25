7669ea3f63a2e89074992104bc6af45dbd47bac1
naidra68 <ardian.crazy@gmail.com>
2025-06-25 18:12:24 +0700

    add function expression and edit index

---
+++ javascript/34-function_express.js
@@ -0,0 +1,13 @@
+//? Function Declaration and Function Expression?
+
+//* function declaration
+// function tampilPesan(nama) {
+//     alert('halo ' + nama);
+// }
+// tampilPesan('Budi');
+
+//* function expression
+var tampilPesan = function (nama) {
+    alert('Halo ' + nama);
+}
+tampilPesan();
