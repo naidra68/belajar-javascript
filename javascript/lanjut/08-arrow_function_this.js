d425ccc68332ee795049b1f964598ddb175e888c
naidra68 <ardian.crazy@gmail.com>
2025-06-28 23:42:33 +0700

    add arrow function with this and edit some index, also add css

---
+++ javascript/lanjut/08-arrow_function_this.js
@@ -0,0 +1,61 @@
+// Konsep this pada arrow function
+
+// Constructor function
+// const Mahasiswa = function() {
+//     this.nama = 'Naidra68';
+//     this.umur = 33;
+//     this.sayHello = function() {
+//         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
+//     }
+// }
+// const naidra68 = new Mahasiswa();
+
+
+
+// Arrow Function 
+
+// const Mahasiswa = function () {
+//     this.nama = 'Naidra68';
+//     this.umur = 33;
+//     this.sayHello = () => {
+//         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
+//     }
+// }
+
+// const naidra68 = new Mahasiswa();
+
+// Object literal
+
+// const mhs1 = {
+//     nama: 'Tri Ardian',
+//     umur: 33,
+//     sayHello: () => {
+//         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
+//     }
+// }
+
+
+
+
+
+// const Mahasiswa = function() {
+//     this.nama = 'Naidra68';
+//     this.umur = 33;
+//     this.sayHello = function() {
+//         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
+//     }
+
+
+//     setInterval(() => {
+//         console.log(this.umur++);
+//     }, 500);
+// }
+// const naidra68 = new Mahasiswa();
+
+
+
+
+
+
+
+
