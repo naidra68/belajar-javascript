24d04b76e63ca74ca41ec4377d90e74666bd01a9
naidra68 <ardian.crazy@gmail.com>
2025-06-28 10:58:05 +0700

    add different types var let and const in javascript, also edit index

---
+++ javascript/lanjut/06-var_let_const.js
@@ -0,0 +1,38 @@
+// function tes() {
+//     for( var i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }
+// tes();
+
+// (function(){
+//     for( var i = 0; i < 10; i++) {
+//         console.log(i);
+//     }
+// }());
+
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(i);
+
+// const i = 10;
+// i = 15;
+
+// const mhs = {
+//     nama : 'Naidra68',
+//     umur : 31
+// }
+// mhs = {
+//     jurusan : 'Teknik Informatika'
+// }
+// mhs.umur = 35;
+// console.log(mhs);
+
+// const i = [1,2,3];
+
+// i.push(4);
+
+// console.log(i);
