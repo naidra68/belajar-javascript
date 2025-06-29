37766ec1d549cea7f05ed8af027e62042f8efabd
naidra68 <ardian.crazy@gmail.com>
2025-06-29 08:42:13 +0700

    add high order function and edit index

---
+++ javascript/lanjut/10-high_order_function.js
@@ -0,0 +1,40 @@
+// High Order Function
+
+
+// Function yang memiliki callback sebagai argument nya maka disebut High Order Function
+
+// Function sebagai argument disebut callback.
+
+// function kerjakanTugas(matakuliah, selesai) {
+//     console.log(`Mulai mengerjakan tugas ${matakuliah} ...`);
+//     selesai()
+// }
+
+// function selesai() {
+//     alert('Selesai mengerjakan tugas!');
+// }
+
+// kerjakanTugas('Pemrogaman Web', selesai);
+
+// setTimeout(function () {
+//     console.log('Hello World');
+// }, 1000);
+
+// let total = 0, count = 1;
+// while (count <= 10) {
+//     total += count;
+//     count += 1;
+// }
+// console.log(total);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+// function repeatLog(n) {
+//     for(let i = 0; i < n; i++) {
+//         console.log(i);
+//     }
+// }
+
+// repeatLog(100);
