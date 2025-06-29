44dfdef37cb2a7f601c49729f74162ce22a0806a
naidra68 <ardian.crazy@gmail.com>
2025-06-29 08:23:55 +0700

    edit index and arrow function

--- javascript/lanjut/08-arrow_function_this.js
+++ javascript/lanjut/08-arrow_function_this.js
@@ -1,6 +1,7 @@
 // Konsep this pada arrow function
 
-// Constructor function
+// Constructor 
+
 // const Mahasiswa = function() {
 //     this.nama = 'Naidra68';
 //     this.umur = 33;
@@ -26,14 +27,25 @@
 
 // Object literal
 
+// Benar
 // const mhs1 = {
 //     nama: 'Tri Ardian',
 //     umur: 33,
-//     sayHello: () => {
+//     sayHello: function () {
 //         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
 //     }
 // }
 
+// Error
+// const mhs1 = {
+//     nama: 'Tri Ardian',
+//     umur: 33,
+//     sayHello: () => {
+//         return `Halo, naam saya ${this.nama}, dan saya ${this.umur} tahun.`;
+//     }
+// }
+
+// console.log(mhs1.sayHello());
 
 
 
@@ -45,6 +57,16 @@
 //         return `Halo, nama saya ${this.nama}, dan saya ${this.umur} tahun.`;
 //     }
 
+
+    // Function Declaration
+    // this didalam sini akan merajuk ke window (global scope).
+
+    // setInterval(function () {
+    //     console.log(this.umur++);
+    // }, 500);
+
+    // Arrow Function
+    // karena arrow function tidak ada konsep this maka dia akan merajuk ke object-nya yaitu Mahasiswa.
 
 //     setInterval(() => {
 //         console.log(this.umur++);
